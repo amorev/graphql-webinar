3f7e358f3fd6be229080b80efb8f5ed318d5c84d
Anton Morev <shikto@mail.ru>
2020-05-22 11:02:40 +0300

    #0 PHP. Simple Graphql handler

--- server.php
+++ server.php
@@ -1,3 +1,32 @@
 <?php
+require_once __DIR__.'/vendor/autoload.php';
 
-echo 1;
+use GraphQL\Type\Definition\ObjectType;
+use GraphQL\Type\Definition\Type;
+use GraphQL\Type\Schema;
+use GraphQL\GraphQL;
+
+$schema = new Schema(
+    [
+        'query' => new ObjectType(
+            [
+                'name' => 'Query',
+                'fields' => [
+                    'hello' => [
+                        'type' => Type::string(),
+                        'args' => [
+                            'name' => Type::string(),
+                        ],
+                        'resolve' => function ($rootValue, $args) {
+                            return "Hello, ".$args['name'];
+                        },
+                    ],
+                ],
+            ]
+        ),
+    ]
+);
+
+$query = 'query { hello(name: "Anton") }';
+
+echo json_encode(GraphQL::executeQuery($schema, $query)->toArray());
