6200fb0a757cec7ce94f20bd53fbd95c359df277
Anton Morev <shikto@mail.ru>
2020-12-01 21:21:16 +0300

    Not forget to explain hot to get Authors field City

--- index.js
+++ index.js
@@ -1,20 +1,77 @@
-var { graphql, buildSchema } = require('graphql');
+var { graphql, buildSchema } = require('graphql')
+var graphqlHTTP = require('express-graphql')
+var express = require('express')
+
+const app = express()
 
 // Construct a schema, using GraphQL schema language
 var schema = buildSchema(`
+  type City {
+    id: ID!
+    title: String
+  }
+  type Author {
+      name: String
+      city: City
+  }
   type Query {
     hello: String
+    bye: String
+    cities: [City]
+    authors: [Author]
+    books: [String]
+    users: [String]
+    talk (name: String!): String
   }
-`);
+`)
+
+const cities = [
+  {
+    id: 1,
+    title: 'Moscow'
+  },
+  {
+    id: 2,
+    title: 'Ulyanovsk',
+  }
+]
+
+const authors = [
+  {
+    id: 1,
+    name: 'Anton',
+    city_id: 1
+  },
+  {
+    id: 2,
+    name: 'Ivan',
+    city_id: 2
+  },
+]
 
 // The root provides a resolver function for each API endpoint
 var root = {
+  Author: {
+    city (args, author) {
+      console.log(args, author)
+      return {}
+    },
+  },
   hello: () => {
-    return 'Hello world!';
+    return 'Hello world!'
+  },
+  cities: () => {
+    return cities
+  },
+  authors: () => {
+    return authors
   }
-};
+}
 
-// Run the GraphQL query '{ hello }' and print out the response
-graphql(schema, '{ hello }', root).then((response) => {
-  console.log(response);
-});
+app.use('/', graphqlHTTP.graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}))
+
+app.listen(3000)
