96c4b30eb4809cd4728dd3bb7290ae1838499362
Anton Morev <shikto@mail.ru>
2021-08-09 21:15:01 +0300

    hardcode caching

--- index.js
+++ index.js
@@ -5,8 +5,7 @@ const {
   GraphQLList,
   GraphQLObjectType,
   GraphQLNonNull,
-  GraphQLSchema
-} = require('graphql')
+  GraphQLSchema} = require('graphql')
 const { Client } = require('pg')
 const client = new Client({
   user: 'root',
@@ -18,7 +17,7 @@ const client = new Client({
 
 const DataLoader = require('dataloader')
 const AuthorLoader = new DataLoader((authorIds) => {
-  let s = "SELECT * from authors where id in (" + authorIds.join(',') +")"
+  let s = 'SELECT * from authors where id in (' + authorIds.join(',') + ')'
   console.log('make query:', ++queriesCount)
   return client.query(s)
     .then(r => r.rows).then(authors => {
@@ -33,57 +32,58 @@ const express = require('express')
 const { graphqlHTTP } = require('express-graphql')
 let posts = [
   {
-    id: "123",
-    title: "title1",
+    id: '123',
+    title: 'title1',
     authorId: 1,
-    body: "body1"
+    body: 'body1'
   },
   {
-    id: "1232",
+    id: '1232',
     authorId: 2,
-    title: "title2",
-    body: "body2"
+    title: 'title2',
+    body: 'body2'
   },
   {
-    id: "1232",
+    id: '1232',
     authorId: 2,
-    title: "title2",
-    body: "body2"
+    title: 'title2',
+    body: 'body2'
   },
   {
-    id: "1232",
+    id: '1232',
     authorId: 2,
-    title: "title2",
-    body: "body2"
+    title: 'title2',
+    body: 'body2'
   },
   {
-    id: "1232",
+    id: '1232',
     authorId: 1,
-    title: "title2",
-    body: "body2"
+    title: 'title2',
+    body: 'body2'
   }
 ]
 let authors = [
   {
-    id: "123",
-    name: "title1"
+    id: '123',
+    name: 'title1'
   },
   {
-    id: "1232",
-    name: "title2"
+    id: '1232',
+    name: 'title2'
   }
 ]
-let queriesCount = 0;
+let queriesCount = 0
+
 function getAuthor (id) {
   console.log('make query:', ++queriesCount)
-  return client.query("SELECT * FROM authors where id = $1", [id])
+  return client.query('SELECT * FROM authors where id = $1', [id])
     .then(res => {
       return res.rows[0]
     })
 }
 
 const PostType = new GraphQLObjectType({
-  "name": "Post",
+  'name': 'Post',
   fields: () => ({
     id: {
       type: new GraphQLNonNull(GraphQLString)
@@ -104,7 +104,7 @@ const PostType = new GraphQLObjectType({
 })
 
 const AuthorType = new GraphQLObjectType({
-  "name": "Author",
+  'name': 'Author',
   fields: () => ({
     id: {
       type: new GraphQLNonNull(GraphQLInt)
@@ -123,9 +123,21 @@ const AuthorType = new GraphQLObjectType({
 })
 
 const query = new GraphQLObjectType({
-  name: "Query",
+  name: 'Query',
   fields: () => {
     return {
+      balance: {
+        type: GraphQLInt,
+        resolve: () => {
+          return 100
+        }
+      },
+      serverTime: {
+        type: GraphQLString,
+        resolve: () => {
+          return (new Date).toISOString()
+        }
+      },
       posts: {
         type: new GraphQLList(PostType),
         resolve: function () {
@@ -141,16 +153,16 @@ const query = new GraphQLObjectType({
             .then(res => {
               const authors = res.rows
               console.log(authors)
-              return authors;
+              return authors
             })
         }
       },
       author: {
         type: AuthorType,
         args: {
-            id:  {
-              type: GraphQLInt
-            }
+          id: {
+            type: GraphQLInt
+          }
         },
         resolve: function (root, args) {
           return getAuthor(args.id)
@@ -164,6 +176,32 @@ const schem = new GraphQLSchema({
   query
 })
 
-const app = express();
+const app = express()
+app.get('/mainpageinfo', (req,res) => {
+  const data = graphql(schem, `{
+  posts {
+    id
+    title
+    body
+    author {
+      id
+      name
+    }
+  }
+  author (id: 1) {
+    id
+  }
+  authors {
+    id
+  }
+  allPosts:posts {
+    id
+  }
+}`)
+    .then(data => {
+      res.send(data)
+    })
+})
 app.use('/', graphqlHTTP({ schema: schem, graphiql: true }))
+
 app.listen(5000)
