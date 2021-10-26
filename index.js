c8efeab96c28f3bf7cecb674a912814f061d5fc1
Anton Morev <shikto@mail.ru>
2021-10-26 20:20:17 +0300

    Inline GraphQL, multiple fields, field with argument

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log(1)
+var gql = require('graphql')
+
+var schema = gql.buildSchema(`
+  type Query {
+    hello: String,
+    bye: String,
+    talk (name: String!): String
+  }
+`);
+
+var root = {
+  hello: () => {
+    return 'Hello World!'
+  },
+  bye: () => {
+    return 'Bye` World!'
+  },
+  talk: (args) => {
+    return 'Talk: ' + args.name
+  }
+}
+
+gql.graphql(schema, '{ hello, bye, talk(name: "Anton") }', root).then((response) => {
+  console.log(response)
+})
