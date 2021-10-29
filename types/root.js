c9b1f0a0b2e7e2b0fcf51b289b2ad2206989b409
Anton Morev <shikto@mail.ru>
2021-10-29 20:21:28 +0300

    cycle deps

--- types/root.js
+++ types/root.js
@@ -5,9 +5,43 @@ const {
   GraphQLNonNull,
   GraphQLString
 } = require('graphql')
-const PostType = require('./post')
 const AuthorType = require('./author')
-const UserType = require('./user')
+const UserType = new GraphQLObjectType({
+  name: 'User',
+  fields: () => ({
+    id: { type: new GraphQLNonNull(GraphQLString) },
+    name: { type: new GraphQLNonNull(GraphQLString) },
+    username: { type: new GraphQLNonNull(GraphQLString) },
+    posts: {
+      type: new GraphQLList(PostType),
+      resolve:async  (user) => {
+        let promise = await axios('https://jsonplaceholder.typicode.com/posts?userId=' + user.id).then(r => r.data)
+        return promise
+      }
+    }
+  })
+})
+const DataLoader = require('dataloader')
+const UsersLoader = new DataLoader((userIds) => {
+  return axios(`https://jsonplaceholder.typicode.com/users`).then(d => d.data.filter(user => userIds.includes(user.id)))
+})
+const PostType = new GraphQLObjectType({
+  name: "Post",
+  description: "This is post object",
+  fields: () => ({
+    id: { type: new GraphQLNonNull(GraphQLString) },
+    title: { type: new GraphQLNonNull(GraphQLString) },
+    body: { type: GraphQLString },
+    user: {
+      type: UserType,
+      resolve: (root) => {
+        return UsersLoader.load(root.userId)
+      }
+    }
+  })
+})
+
+
 const axios = require('axios')
 
 module.exports = new GraphQLObjectType({
@@ -17,7 +51,7 @@ module.exports = new GraphQLObjectType({
       type: new GraphQLList(PostType),
       resolve: () => {
         return axios.get('https://jsonplaceholder.typicode.com/posts')
-          .then(r => r.data)
+          .then(r => r.data.slice(0, 3))
       }
     },
     authors: {
